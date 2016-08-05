24ab9b85e13bc846acd7c928d48a148878d45090
Jura Khrapunov <jura@khrapunov.com>
2016-08-05 11:36:41 -0400

    Styling update

--- ecisstoriesmap/app.js
+++ ecisstoriesmap/app.js
@@ -7,35 +7,44 @@ L.tileLayer('http://korona.geog.uni-heidelberg.de/tiles/roads/x={x}&y={y}&z={z}'
 	attribution: 'Imagery from <a href="http://giscience.uni-hd.de/">GIScience Research Group @ University of Heidelberg</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-var markers = L.markerClusterGroup({
-  showCoverageOnHover: false  
-});
 
-$.getScript('https://www.gstatic.com/charts/loader.js', function(){
-    google.charts.load('current', {packages: ['corechart']});
-    google.charts.setOnLoadCallback(function(){
-      var queryString = encodeURIComponent('SELECT B,C,D,E,F,H,I,J,L WHERE E is not null OFFSET 4');
-      var query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/1hzj2e1CREa_V5HmJxOSehadHU08QzPC-IkqlA48QhpQ/gviz/tq?sheet=1stGen&tq=' + queryString);
-      query.send(addMarkers);      
-    });
+// get project data from google spreadsheet
+google.charts.load('current', {packages: ['corechart']});
+google.charts.setOnLoadCallback(function(){
+  var queryString = encodeURIComponent('SELECT B,C,D,E,F,H,I,J,L WHERE E is not null OFFSET 4');
+  var query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/1hzj2e1CREa_V5HmJxOSehadHU08QzPC-IkqlA48QhpQ/gviz/tq?sheet=1stGen&tq=' + queryString);
+  query.send(addMarkers);      
 });
 
 function addMarkers(response) {
   var data = response.getDataTable();
   var rows = data.getNumberOfRows();
+  var markers = L.markerClusterGroup({
+    showCoverageOnHover: false  
+  });
   for (var i = 0; i < rows; i++) {
     var coords = data.getValue(i, 6).split(',');
     if (coords.length == 2) {
       var latLng = L.latLng(parseFloat(coords[0].trim()), parseFloat(coords[1].trim()));
+      var popup = '<div><h1>' + data.getValue(i, 3) + '</h1>';
+      if (data.getValue(i, 8) != null) {
+        popup += '<img src="' + data.getValue(i, 8) + '">';
+      }
+      popup += '<p>' + data.getValue(i, 4) + '</p>';
+      if (data.getValue(i, 5) != null) {
+        popup += '<p><a href="' + data.getValue(i, 5) + '" target="_blank">read more</a></p>';
+      }
+      popup += '</div>';
       var options = {
-        title: data.getValue(i, 3),
+        title: data.getValue(i, 1),
         icon: (data.getValue(i, 7) != null) ? L.icon({
-          iconUrl: 'thumbs/' + data.getValue(i, 7).trim(),
+          iconUrl: 'img/' + data.getValue(i, 7).trim(),
           iconSize: [75,75],
-          className: 'marker-icon'
+          className: 'marker-icon gen-' + data.getValue(i, 2)
         }) : new L.Icon.Default()
       };
-      markers.addLayer(L.marker(latLng, options));
+      var marker = L.marker(latLng, options).bindPopup(popup);
+      markers.addLayer(marker);
     }
   }                
   map.addLayer(markers);
